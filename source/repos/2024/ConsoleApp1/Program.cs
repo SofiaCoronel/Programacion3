6fffeada203e4d74b492f6e8b4356a1345f7216e
SofiaCoronel <sofia.coronel.2001@gmail.com>
2024-04-08 22:39:45 -0300

    Unit1: Consola, Class, Reference

--- source/repos/2024/ConsoleApp1/Program.cs
+++ source/repos/2024/ConsoleApp1/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace ConsoleApp1
+﻿using Starwars.Core.Entities;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            var p1 = new Person();
+            
+            bool b1 = true;
+            Boolean b2 = true;
+
+            int num1 = 1; 
+            System.Int32 num2 = 1;
         }
     }
 }
--- source/repos/2024/Starwars.Core.Entities/Person.cs
+++ source/repos/2024/Starwars.Core.Entities/Person.cs
@@ -1,4 +1,4 @@
-﻿namespace ConsoleApp1
+﻿namespace Starwars.Core.Entities
 {
     public class Person
     {
